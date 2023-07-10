afb4df6c996127b503c3dbca584ac44a78703739
mollinaca <mail@mollinaca.dev>
2023-07-10 22:59:00 +0900

    update

---
+++ app/config.py
@@ -0,0 +1,5 @@
+target = {
+    "ヨークマート": "https://www.york-inc.com/store/oomiyaminaminakano.html",
+    "業務スーパー": "https://www.gyomusuper.jp/bargain/index.php",
+    "マツモトキヨシ": "https://www.shufoo.net/pntweb/shopDetail/871298/"
+}
--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+import json
 import lib
+import config
 
 def main():
+
+    TARGET = config.target
+    print (TARGET)
+
+    LAST_JSON_PATH = "data/last.json"
+    with open(LAST_JSON_PATH) as f:
+      last_json = json.load(f)
+
+    for shop in TARGET:
+      print (shop, TARGET[shop])
+
 
     slack_clinet = lib.SlackAPI()
     res = slack_clinet.iwebhook("test message")
