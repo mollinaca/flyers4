f59034c8b24f5ac85665c22267b6c03b84daa7a0
mollinaca <mail@mollinaca.dev>
2024-02-24 22:00:47 +0900

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+set -eu
 
 if [ ! -e $(pwd)/app/data/last.json ]; then
     cp $(pwd)/app/data/last-init.json $(pwd)/app/data/last.json
 fi
 
 PATH=$PATH:/usr/bin/:/usr/local/bin
-# docker-compose up
 sudo docker compose run flyers4 python main.py
